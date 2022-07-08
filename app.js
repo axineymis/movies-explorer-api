9a0600909a1300fae3fda5b7f51ba9693923cb77
axineymis <otm-natasha.biryukova@yandex.ru>
2022-07-09 01:14:19 +0300

    creating routes

---
+++ app.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const { errors } = require('celebrate');
+
+const { PORT = 3001 } = process.env;
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/moviesdb', {
+  useNewUrlParser: true,
+});
+
+const cors = require('./middlewares/cors');
+const routes = require('./routes');
+const limiter = require('./middlewares/limiter');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const errorCatcher = require('./errors/errorCatcher');
+
+app.use(cors);
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.use('/', limiter);
+
+routes(app);
+
+app.use(errorLogger);
+app.use(errors());
+app.use(errorCatcher);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ routes/users.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const {
+  patchProfile,
+  getMe,
+} = require('../controllers/users');
+
+const { patchProfileValidityCheck } = require('../middlewares/validation');
+
+router.get('/me', getMe);
+router.patch('/me', patchProfileValidityCheck, patchProfile);
+
+module.exports = router;
